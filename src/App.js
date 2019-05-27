79366f0f08ea4b701f93c4f0fec7848e82112767
samuelmo <samuelmo78@gmail.com>
2019-05-27 01:55:59 -0600

    create checkbox and placeholder text

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return(
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  )
+}
+
+export default App

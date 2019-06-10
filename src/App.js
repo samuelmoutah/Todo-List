5fa757db296f070b59cdafbfb82c77b189ae2951
samuelmo <samuelmo78@gmail.com>
2019-06-10 01:47:34 -0600

    practice setState

--- src/App.js
+++ src/App.js
@@ -8,11 +8,26 @@ class App extends Component {
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
   }
   
+  handleChange(id) {
+    this.setState(prevState => {
+      const updateState = prevState.todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updateState
+      }
+    })
+  }
+
   render() {
 
-    const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} todos={item}/>)
+    const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} todos={item} handleChange={this.handleChange}/>)
 
     return(
       <div className="todo-list">
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,10 @@ import React from 'react'
 function TodoItem(props) {
     return(
         <div className="todo-item">
-            <input type="checkbox" checked={props.todos.completed}/>
+            <input 
+                type="checkbox" 
+                checked={props.todos.completed}
+                onChange={(event) => {props.handleChange(props.todos.id)}}/>
             <p>{props.todos.text}</p>
         </div>
     )
